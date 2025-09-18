10b6c77ad07cfde04ff3dbadee2d7421a7fdd955
Harry <thcstrandainghiathinh6a1@gmail.com>
2025-09-18 12:47:11 +0700

    fix homepage

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,7 @@
 // router.jsx
 import { createBrowserRouter } from "react-router-dom";
 import StoryBook from "./components/StoryBook";
-import HomePage from "./pages/HomePage"; // ví dụ bạn có HomePage
+import HomePage from "./pages/Homepage"; // ví dụ bạn có HomePage
 
 const router = createBrowserRouter([
   {
